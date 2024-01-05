4736fe4012a07003e816978150835e5054fba1ab
Lubna Intezar <lubna.khan2609@gmail.com>
2024-01-05 11:45:16 +0000

    basic validation done

--- Banking_System.py
+++ Banking_System.py
@@ -1,5 +1,6 @@
 import getpass
 import math
+import tkinter
 
 correct_username='lubna'
 correct_password='li123'
@@ -7,74 +8,93 @@ balance=6000.0
 
 while True:
     
-    user_name=input("Enter the username please :")
-    password=getpass.getpass("Enter your password :")
+            user_name=input("Enter the username please :")
+            password=getpass.getpass("Enter your password :")
+        
+            if correct_username == user_name and correct_password == password:
+                print("Username and Password are validated")
+                break
+        
+            else:
+                print("please, Enter correct details:")   
     
-    if correct_username == user_name and correct_password == password:
-        print("username and password are validated")
-        break
-    
-    else:
-        print("please, Enter correct details:")    
 while True:
-    #user selection:
-    print("")
-    
-    print("Banking basic operations:")
-    print("Enter '1' for checking Balance:")
-    print("Enter '2' for Transfering Funds:")
-    print("Enter '3' for Depositing Money:")
-    print("Enter '4' for Withdrawing Money:")
-    print("Enter '5' for Viewing Balance Money:") 
-    print("Enter '0' to Exit")
-    choice=input("Enter your choice:")
-    
-     
-    if choice == '1':
+    try:
+            print("")
+            print("Banking basic operations:")
+            print("")
+            print("Enter '1' for checking Balance:")
+            print("Enter '2' for Transfering Funds:")
+            print("Enter '3' for Depositing Money:")
+            print("Enter '4' for Withdrawing Money:")
+            print("Enter '5' for Viewing Balance Money:") 
+            print("Enter '0' to Exit")
+            print("")
 
-        print("{} your balance is :{}".format(user_name,float(balance)))
+            choice=input("Enter your choice:")
+            print("")
+          
+            if choice == '1':
 
-    elif choice == '2':
+                print("{} your balance is :{}".format(user_name,float(balance)))
+                print("")
 
-        reciepient=input("Enter the reciepient Account number: ")  
-        tranfer_amount=float(input("Enter the amount to be tranfer:"))
-        if balance > tranfer_amount:
+            elif choice == '2':
 
-            #current_balance=balance - tranfer_amount
-            balance -= tranfer_amount
-            print("Amount £{} transfered to '{}' account number,Successfully".format(tranfer_amount,reciepient))
-            print("{} your current balance is : {}".format(user_name,float(balance)))
-        else:
-            print("Insufficient Amount: Sorry") 
+                reciepient_Account=input("Enter the reciepient Account number: ")  
+                tranfer_amount=float(input("Enter the amount to be tranfer:"))    
+                if balance > tranfer_amount:
 
-    elif choice == '3':
+                    balance -= tranfer_amount
+                    print("Amount £{} transfered to '{}' account number,Successfully".format(tranfer_amount,reciepient_Account))
+                    print("{} your current balance is : {}".format(user_name,float(balance)))
+                    print("")
+                else:
+                    print("Insufficient Amount: Sorry") 
 
-        deposit=float(input("Enter the amount to deposite: ")) 
-        if deposit > 0:
-            
-            #current_balance+= deposit
-            balance+= deposit
-            print("successfully money{} deposited:".format(deposit))
-            #print("{} your current balance is {} ".format(user_name,current_balance))
-            print("{} your current balance is {} ".format(user_name,balance))
-        else:
-            print("invalid input")
+            elif choice == '3':
 
-    elif  choice == '4':
+                deposit=float(input("Enter the amount to deposite: ")) 
+                if deposit > 0:
+                    
+                    #current_balance+= deposit
+                    balance+= deposit
+                    print("successfully money{} deposited:".format(deposit))
+                    #print("{} your current balance is {} ".format(user_name,current_balance))
+                    print("{} your current balance is {} ".format(user_name,balance))
+                    print("")
+                else:
+                    print("Invalid input,Retry again")
 
-        withdraw=float(input("Enter the amount to withdraw: "))
-        if balance > withdraw:
-            #current_balance -= withdraw
-            balance -= withdraw
-            print("{} your current balance is {}".format(user_name,balance) )
-            #print("{} your current balance is {}".format(user_name,current_balance) )
+            elif  choice == '4':
 
-    elif choice == '5':
-        print("Viewing balance money is {}".format(balance))
-        # print("Viewing balance money is {}".format(current_balance))
-    elif choice == '0':
-        print("Exit")
-        break    
+                withdraw=float(input("Enter the amount to withdraw: "))
+                if balance > withdraw:
+                    balance -= withdraw
+                    print("{} your current balance is {}".format(user_name,balance) )  
+                    print("")
+                else :   
+                     raise Exception("Insufficient Amount")    
+                
+            elif choice == '5':
+                print("Viewing balance money is {}".format(balance))
+                
+            elif choice == '0':
+                print("Exit")
+                break   
+    except ValueError:
+                print("Please, Enter numeric input,Try again")    
+    except Exception as e:
+
+        print(e)
+
+
+
+
+
+
+
+             
 
 
 
